aad796e750272a38df7637c8798db56ad0e99981
Georgios.D <georgios_97@hotmail.com>
2023-06-30 23:08:36 +0200

    Fixed bug with navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,77 +7,44 @@ const Navbar = () => {
     const handleClick = () => setNav(!nav);
 
     return (
-        <div className="w-full h-[80px] flex items-center justify-between top-0 z-10 bg-white fixed drop-shadow-lg px-5">
-            <div className="flex items-center">
-                <h1 className="text-3xl font-bold mr-4 sm:text-4xl">DocSpot</h1>
-                <ul className="hidden md:flex">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Support</li>
-                    <li>Platforms</li>
-                    <li>Pricing</li>
-                </ul>
-            </div>
-            <div>
-                <div className="hidden md:flex pr-4">
-                    <button className="border-none bg-transparent text-black mr-4">
-                        Sign In
-                    </button>
-                    <button className="px-8 py-3">Register</button>
+        <div className="w-screen h-auto bg-white fixed drop-shadow-lg top-0 z-10">
+            <div className="flex items-center justify-between p-5">
+                <div className="flex items-center">
+                    <h1 className="text-3xl font-bold mr-4 sm:text-4xl">
+                        DocSpot
+                    </h1>
+                    <ul className="hidden md:flex">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Support</li>
+                        <li>Platforms</li>
+                        <li>Pricing</li>
+                    </ul>
                 </div>
-                <div className="md:hidden" onClick={handleClick}>
-                    {!nav ? (
-                        <Bars3Icon className="w-5" />
-                    ) : (
-                        <XMarkIcon className="w-5" />
-                    )}
+                <div>
+                    <div className="hidden md:flex pr-4">
+                        <button className="border-none bg-transparent text-black mr-4">
+                            Sign In
+                        </button>
+                        <button className="px-8 py-3">Register</button>
+                    </div>
+                    <div className="md:hidden" onClick={handleClick}>
+                        {!nav ? (
+                            <Bars3Icon className="w-5" />
+                        ) : (
+                            <XMarkIcon className="w-5" />
+                        )}
+                    </div>
                 </div>
             </div>
+            <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full"}>
+                <li className="border-b-2 border-zinc-300 w-full">Home</li>
+                <li className="border-b-2 border-zinc-300 w-full">About</li>
+                <li className="border-b-2 border-zinc-300 w-full">Support</li>
+                <li className="border-b-2 border-zinc-300 w-full">Platforms</li>
+                <li className="border-b-2 border-zinc-300 w-full">Pricing</li>
+            </ul>
         </div>
-        // <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
-        //     <div className="px-2 flex items-center justify-between w-full h-full">
-        //         <div className="flex items-center">
-        //             <h1 className="text-3xl font-bold mr-4 sm:text-4xl">
-        //                 DocSpot
-        //             </h1>
-        //             <ul className="hidden md:flex">
-        //                 <li>Home</li>
-        //                 <li>About</li>
-        //                 <li>Support</li>
-        //                 <li>Platforms</li>
-        //                 <li>Pricing</li>
-        //             </ul>
-        //             <div className="hidden md:flex pr-4">
-        //                 <button className="border-none bg-transparent text-black mr-4">
-        //                     Sign In
-        //                 </button>
-        //                 <button className="px-8 py-3">Register</button>
-        //             </div>
-        //             <div className="md:hidden" onClick={handleClick}>
-        //                 {!nav ? (
-        //                     <Bars3Icon className="w-5" />
-        //                 ) : (
-        //                     <XMarkIcon className="w-5" />
-        //                 )}
-        //             </div>
-        //         </div>
-        //     </div>
-        //     <ul
-        //         className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8"}
-        //     >
-        //         <li className="border-b-2 border-zinc-300 w-full">Home</li>
-        //         <li className="border-b-2 border-zinc-300 w-full">About</li>
-        //         <li className="border-b-2 border-zinc-300 w-full">Support</li>
-        //         <li className="border-b-2 border-zinc-300 w-full">Platforms</li>
-        //         <li className="border-b-2 border-zinc-300 w-full">Pricing</li>
-        //         <div className="flex flex-col my-4">
-        //             <button className="bg-transparent text-indigo-600 px-8 py-3 mb-4">
-        //                 Sign in
-        //             </button>
-        //             <button className="px-8 py-3">Register</button>
-        //         </div>
-        //     </ul>
-        // </div>
     );
 };
 
